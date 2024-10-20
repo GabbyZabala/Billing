251267f3b8bd7deab7d96bf74ce9fa91fc0a4a9c
Aly <alliahtradio@gmail.com>
2024-10-20 16:51:44 +0800

    Update ui.java

--- ui.java
+++ ui.java
@@ -1,3 +1,32 @@
 public class ui{
-    
+    public static void main_Menu{
+
+        do {
+
+            System.out.println("== Hospital Billing=======");
+            System.out.println("-----------Menu----------");
+            System.out.println("1.Consultation Services");
+            System.out.println("2.Laboratory Services");
+            System.out.println("3.Room and Admission Services");
+            System.out.println("5.Exit");
+
+            choice=
+
+
+        
+                
+        
+        
+        }
+
+
+
+
+
+
+
+
+
+
+    }
 }
