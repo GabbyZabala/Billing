19094f2569d4da9bf8485558e5b694a8ee28dc7f
MacCloud <kuzabala@my.cspc.edu.ph>
2024-10-20 23:37:30 +0800

    Exiting done
    +Exit function complete

--- Grilling.java
+++ Grilling.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-public class Grilling extends ui{
-    Boolean loop = true;
-    public static void clear() {
-        if (System.getProperty("os.name").contains("Windows")) {
+public class Grilling extends ui {
+
+    public static void clear() {// clear function
+        if (System.getProperty("os.name").contains("Windows")) {// for console using
             // For Windows
             System.out.print("\033c");
         } else {
@@ -13,8 +13,24 @@ public class Grilling extends ui{
         }
     }
 
+    public static void animation(String input) {
+        for (int i = 0; i < input.length(); ++i) {
+            System.out.print(input.charAt(i));
+            sleep(500); // Adjust the delay in milliseconds
+        }
+    }
+
+    public static void sleep(int numbers) {// sleep function
+        try {
+            // Sleep for 2 seconds
+            Thread.sleep(numbers);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void Display_Screen(int screen) {
-        switch (screen){
+        switch (screen) {
             case 0:
                 main_Menu();
                 break;
@@ -27,31 +43,57 @@ public class Grilling extends ui{
                 break;
             case 4:
                 break;
-            
             default:
+
                 break;
-            
         }
     }
-	public static void main(String[] args) {
-        int locale = 0, revert;
-        while(true){  
+
+    public static void main(String[] args) {
+        animation("Loading.....");
+        clear();
+        Boolean loop = true;
+        int locale = 0;
+        int revert;
+        while (loop) {
             revert = locale;
             header();
-            Scanner command= new Scanner(System.in);
+            Scanner command = new Scanner(System.in);
             Display_Screen(locale);
             System.err.print("\tEnter choice: -->\t");
             int Terminate = command.nextInt();
             switch (revert) {
                 case 0:
-                    
+                    if (revert == locale) {
+                        switch (Terminate) {
+                            case 1:
+                                // goto consultation
+                                break;
+                            case 2:
+                                // goto laboratory
+                                break;
+                            case 3:
+                                // goto room and admin
+                                break;
+                            case 4:
+                                // transaction records
+                                break;
+                            case 5:// exit
+                                clear();
+                                sleep(1000);
+                                animation("Exiting programm...");
+                                loop = false;
+                                break;
+                        }
+                    }
                     break;
-                default:
-                    throw new AssertionError();
+                case 1:
+
+                    break;
             }
             locale = Terminate;
 
             clear(); // Clear the console
         }
-	}
+    }
 }
