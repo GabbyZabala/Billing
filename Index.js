9498fe0eb1d36e91920c903ae738ff7c1dc4e49f
MacCloud <135106327+GabbyZabala@users.noreply.github.com>
2024-10-20 16:26:56 +0800

    REstart
    add ui new feature

--- Index.js
+++ Index.js
@@ -1,4 +1,5 @@
-public class Index{
+public class Index extends ui{
+
     public static void clear() {
         if (System.getProperty("os.name").contains("Windows")) {
             // For Windows
@@ -8,10 +9,32 @@ public class Index{
             System.out.print("\033[H\033[2J"); // ANSI escape code for clearing
             System.out.flush();
         }
+    }
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
-        clear(); // Clear the console
+        int locale = 1;
+        Boolean loop = true;
+        while(true){
+            switch (locale){
+                case 0:
+    
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+    
+                    break;
+                
+            }
+            clear(); // Clear the console
+        }
 	}
 }
